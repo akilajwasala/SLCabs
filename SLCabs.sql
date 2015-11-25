69f6b8053457bd2209447756578fd70d3ad78a9a
Adniwhack <rasnayake.kavinda@gmail.com>
2015-11-25 11:41:13 +0530

    Modified Database
    Added registered customer and payment tables

--- SLCabs.sql
+++ SLCabs.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.2.11
+-- version 4.4.12
 -- http://www.phpmyadmin.net
 --
--- Host: localhost
--- Generation Time: Nov 19, 2015 at 08:22 AM
--- Server version: 5.6.21
--- PHP Version: 5.6.3
+-- Host: 127.0.0.1
+-- Generation Time: Nov 25, 2015 at 07:09 AM
+-- Server version: 5.6.25
+-- PHP Version: 5.6.11
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -14,20 +14,20 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `SLCabs`
+-- Database: `slcabs`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `CustomerDetails`
+-- Table structure for table `customerdetails`
 --
 
-CREATE TABLE IF NOT EXISTS `CustomerDetails` (
-`customerID` int(20) NOT NULL,
+CREATE TABLE IF NOT EXISTS `customerdetails` (
+  `customerID` int(20) NOT NULL,
   `fullName` varchar(20) NOT NULL,
   `email` varchar(20) NOT NULL,
   `country` varchar(20) NOT NULL,
@@ -38,10 +38,10 @@ CREATE TABLE IF NOT EXISTS `CustomerDetails` (
 ) ENGINE=InnoDB AUTO_INCREMENT=29 DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `CustomerDetails`
+-- Dumping data for table `customerdetails`
 --
 
-INSERT INTO `CustomerDetails` (`customerID`, `fullName`, `email`, `country`, `passportNo`, `contactNo`, `fax`, `comments`) VALUES
+INSERT INTO `customerdetails` (`customerID`, `fullName`, `email`, `country`, `passportNo`, `contactNo`, `fax`, `comments`) VALUES
 (25, 'Akila wasala', 'akilajwasala@gmail.c', 'Sri Lanka', '922884056', 713943434, 2345678, 'Samaple'),
 (26, 'Sumith Kumara', 'sumi@gmail.com', 'Sri Lanka', '3456789', 34567890, 799778, 'Fast'),
 (27, 'Mihiran', 'mihira@yahoo.com', 'England', '4567', 34567, 33566, 'Final'),
@@ -50,16 +50,45 @@ INSERT INTO `CustomerDetails` (`customerID`, `fullName`, `email`, `country`, `pa
 -- --------------------------------------------------------
 
 --
--- Table structure for table `Reservation`
+-- Table structure for table `payment`
 --
 
-CREATE TABLE IF NOT EXISTS `Reservation` (
-  `reservationID` int(10) NOT NULL,
-  `vehicleID` int(10) NOT NULL,
+CREATE TABLE IF NOT EXISTS `payment` (
+  `PaymentId` int(11) NOT NULL,
+  `ReservationID` int(11) NOT NULL,
+  `DropOffDate` date NOT NULL,
+  `DropOffTime` time NOT NULL,
+  `GrossCost` float NOT NULL,
+  `AdditionalCost` float DEFAULT NULL,
+  `NetCost` float NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `registered_customer`
+--
+
+CREATE TABLE IF NOT EXISTS `registered_customer` (
+  `CustomerID` int(11) NOT NULL,
+  `CustomerEmail` varchar(60) NOT NULL,
+  `CustomerPassword` varchar(64) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `reservation`
+--
+
+CREATE TABLE IF NOT EXISTS `reservation` (
+  `reservationID` int(20) NOT NULL,
+  `customerID` int(20) NOT NULL,
+  `vehicleID` int(20) NOT NULL,
   `pickUpDate` date NOT NULL,
-  `pickUpTime` int(10) NOT NULL,
+  `pickUpTime` time NOT NULL,
   `dropOffDate` date NOT NULL,
-  `dropOffTime` int(10) NOT NULL,
+  `dropOffTime` time NOT NULL,
   `noOfPassengers` int(10) NOT NULL,
   `needADriver` tinyint(1) NOT NULL,
   `pickupLocation` varchar(20) NOT NULL,
@@ -69,13 +98,14 @@ CREATE TABLE IF NOT EXISTS `Reservation` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `VehicleDetails`
+-- Table structure for table `vehicledetails`
 --
 
-CREATE TABLE IF NOT EXISTS `VehicleDetails` (
+CREATE TABLE IF NOT EXISTS `vehicledetails` (
   `vehicleID` int(20) NOT NULL,
   `vehicleName` varchar(20) NOT NULL,
   `vehicleType` varchar(20) NOT NULL,
+  `registrationNo` varchar(20) NOT NULL,
   `seatingCapacity` int(10) NOT NULL,
   `engineCapacity` varchar(10) NOT NULL,
   `vehicleOptions` varchar(10) NOT NULL,
@@ -90,26 +120,53 @@ CREATE TABLE IF NOT EXISTS `VehicleDetails` (
 --
 
 --
--- Indexes for table `CustomerDetails`
+-- Indexes for table `customerdetails`
 --
-ALTER TABLE `CustomerDetails`
- ADD PRIMARY KEY (`customerID`);
+ALTER TABLE `customerdetails`
+  ADD PRIMARY KEY (`customerID`);
 
 --
--- Indexes for table `VehicleDetails`
+-- Indexes for table `payment`
 --
-ALTER TABLE `VehicleDetails`
- ADD PRIMARY KEY (`vehicleID`);
+ALTER TABLE `payment`
+  ADD PRIMARY KEY (`PaymentId`);
+
+--
+-- Indexes for table `reservation`
+--
+ALTER TABLE `reservation`
+  ADD PRIMARY KEY (`reservationID`);
+
+--
+-- Indexes for table `vehicledetails`
+--
+ALTER TABLE `vehicledetails`
+  ADD PRIMARY KEY (`vehicleID`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT for table `CustomerDetails`
+-- AUTO_INCREMENT for table `customerdetails`
 --
-ALTER TABLE `CustomerDetails`
-MODIFY `customerID` int(20) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=29;
+ALTER TABLE `customerdetails`
+  MODIFY `customerID` int(20) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=29;
+--
+-- AUTO_INCREMENT for table `payment`
+--
+ALTER TABLE `payment`
+  MODIFY `PaymentId` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `reservation`
+--
+ALTER TABLE `reservation`
+  MODIFY `reservationID` int(20) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `vehicledetails`
+--
+ALTER TABLE `vehicledetails`
+  MODIFY `vehicleID` int(20) NOT NULL AUTO_INCREMENT;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
